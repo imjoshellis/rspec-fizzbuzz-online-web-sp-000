8e2916256cc104723eb1352a15d6a55b2d444415
Josh Ellis <joshe523@gmail.com>
2020-03-02 03:06:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  fizz = ""
+  buzz = ""
+  if n % 3 == 0 
+    fizz = "Fizz"
+  end
+    
+  if n % 5 == 0
+    buzz = "Buzz"
+  end
+  
+  if fizz+buzz != ""
+    return fizz+buzz
+  else 
+    return NIL
+  end
+  
+end
