4046bce91e6e9bf887247154d3ea80a21b8d18b1
Josh Ellis <joshe523@gmail.com>
2020-03-02 03:09:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(n)
+def fizzbuzz(int)
   fizz = ""
   buzz = ""
   
-  if n % 3 == 0 
+  if int % 3 == 0 
     return "Fizz"
   end
     
-  if n % 5 == 0
+  if int % 5 == 0
     buzz = "Buzz"
   end
   
